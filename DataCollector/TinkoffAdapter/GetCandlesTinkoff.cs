8260a87c670e7ba5f29c0746bd93074d1dcf302e
BlankaKorvo <BlankaKorvo@gmail.com>
2021-12-16 17:25:23 +0300

    Create GetCandlesTinkoff.cs

---
+++ DataCollector/TinkoffAdapter/GetCandlesTinkoff.cs
@@ -0,0 +1,16 @@
+﻿using MarketDataModules.Models.Candles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCollector.TinkoffAdapter
+{
+    internal class GetCandlesTinkoff : CandlesList
+    {
+        public GetCandlesTinkoff(string figi, CandleInterval interval, List<CandleStructure> candles) : base(figi, interval, candles)
+        {
+        }
+    }
+}
