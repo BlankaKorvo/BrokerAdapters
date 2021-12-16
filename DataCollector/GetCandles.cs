1cb69469b152bb650817f75e7d9a981091334221
BlankaKorvo <BlankaKorvo@gmail.com>
2021-12-16 21:50:42 +0300

    refactoring

---
+++ DataCollector/GetCandles.cs
@@ -0,0 +1,29 @@
+﻿using MarketDataModules.Models;
+using MarketDataModules.Models.Candles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCollector.TinkoffAdapter
+{
+    internal class GetCandles : CandlesList
+    {
+        public GetCandles(string figi, CandleInterval interval, List<CandleStructure> candles) : base(figi, interval, candles)
+        {
+        }
+
+        static public async Task<CandlesList> GetCandlesAsync(string figi, CandleInterval candleInterval, int candlesCount, Provider providers = Provider.Tinkoff)
+        {
+            switch (providers)
+            {
+                case Provider.Tinkoff:
+                    //return await TinkoffCandles(figi, candleInterval, candlesCount);
+                case Provider.Finam:
+                    return null;
+            }
+            return null;
+        }
+    }
+}
