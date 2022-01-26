8caa054f7c60b2a13e37ccacae384880cc8b6b63
BlankaKorvo <BlankaKorvo@gmail.com>
2022-01-26 09:59:28 +0300

    fix tests

--- Tests/DataCollector.Tests/GetTinkoffCandlesBase.cs
+++ Tests/DataCollector.Tests/GetTinkoffCandlesBase.cs
@@ -1,4 +1,5 @@
-﻿using MarketDataModules;
+﻿using DataCollector.RetryPolicy;
+using MarketDataModules;
 using MarketDataModules.Candles;
 using System;
 using System.Threading.Tasks;
@@ -8,21 +9,27 @@ namespace DataCollector.Tests
     public class GetTinkoffCandlesBase
     {
         string figi = "BBG000BVPV84";
-        DateTime dateFrom = default;
+        DateTime dateFrom = DateTime.Now;
+
         protected async Task<ICandlesList> GetCandles(CandleInterval candleInterval, DateTime dateTime)
         {
             var candles = await GetMarketData.GetCandlesAsync(figi, candleInterval, dateTime, Provider.Tinkoff);
             return candles;
         }
-        //protected async Task<Tinkoff.Trading.OpenApi.Models.CandleList> GetRealCandles(Tinkoff.Trading.OpenApi.Models.CandleInterval candleInterval)
-        //    => candleInterval switch
-        //    {
-        //        Tinkoff.Trading.OpenApi.Models.CandleInterval.Minute => dateFrom = DateTime.Now.AddHours(-23)
-        //        _ => null
-        //    };
-
-        //    return await TinkoffAdapter.Authorisation.Context.MarketCandlesAsync(figi, DateTime.Now.AddHours(-23), DateTime.Now, candleInterval);
-        //}
+        protected async Task<Tinkoff.Trading.OpenApi.Models.CandleList> GetRealCandles(Tinkoff.Trading.OpenApi.Models.CandleInterval candleInterval)
+        {
+            if (candleInterval < Tinkoff.Trading.OpenApi.Models.CandleInterval.Day)
+                dateFrom = dateFrom.AddHours(-1);
+            else if (candleInterval == Tinkoff.Trading.OpenApi.Models.CandleInterval.Day)
+                dateFrom = dateFrom.AddDays(-2);
+            else if (candleInterval == Tinkoff.Trading.OpenApi.Models.CandleInterval.Week)
+                dateFrom = dateFrom.AddDays(-14);
+            else
+                dateFrom = dateFrom.AddMonths(-2);
+            return await PollyRetrayPolitics.Retry().ExecuteAsync(async () => 
+                            await PollyRetrayPolitics.RetryToManyReq().ExecuteAsync(async () => 
+                            await TinkoffAdapter.Authorisation.Context.MarketCandlesAsync(figi, dateFrom, DateTime.Now, candleInterval)));
+        }
 
         protected async Task<ICandlesList> GetCandles(CandleInterval candleInterval, int candleCount)
         {
