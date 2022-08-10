298bff1664e149bb0d7b628e8dd52a97c7547e53
BlankaKorvo <BlankaKorvo@gmail.com>
2022-08-10 11:18:39 +0300

    Update Class1.cs

--- DataCollector/TinkoffAdapterRPC/Class1.cs
+++ DataCollector/TinkoffAdapterRPC/Class1.cs
@@ -4,11 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grpc.Core;
+using Google.Protobuf.WellKnownTypes;
+using Tinkoff.InvestApi.V1;
+using Tinkoff.InvestApi;
 
 namespace DataCollector.TinkoffAdapterRPC
 {
     internal class Class1
     {
-
+        void Method()
+        {
+            CallInvoker callInvoker = null;
+            var client = new InvestApiClient(callInvoker);
+            var request = new GetLastPricesRequest
+            {
+                Figi = { "BBG0013HGFT4" }
+            };
+            var response = client.MarketData.GetLastPrices(request);
+            foreach (var lastPrice in response.LastPrices)
+            {
+                Console.WriteLine(lastPrice.Price); // 61.62
+            }
+        }
     }
 }
