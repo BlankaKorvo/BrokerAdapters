7762cc4cad8d3c248a05b739c53d0fe94b2a71e0
BlankaKorvo <BlankaKorvo@gmail.com>
2022-12-02 17:06:18 +0300

    update

--- DataCollector/TinkoffAdapterRPC/Class1.cs
+++ DataCollector/TinkoffAdapterRPC/Class1.cs
@@ -1,31 +1,25 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Grpc.Core;
-using Google.Protobuf.WellKnownTypes;
-using Tinkoff.InvestApi.V1;
-using Tinkoff.InvestApi;
+﻿//using Microsoft.Extensions.DependencyInjection;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Grpc.Core;
+//using Google.Protobuf.WellKnownTypes;
+//using Tinkoff.InvestApi.V1;
+//using Tinkoff.InvestApi;
+//using System.IO;
 
-namespace DataCollector.TinkoffAdapterRPC
-{
-    internal class Class1
-    {
-        void Method()
-        {
-            CallInvoker callInvoker = null;
-            var client = new InvestApiClient(callInvoker);
-            var request = new GetLastPricesRequest
-            {
-                Figi = { "BBG0013HGFT4" }
-            };
-            var response = client.MarketData.GetLastPrices(request);
-            foreach (var lastPrice in response.LastPrices)
-            {
-                Console.WriteLine(lastPrice.Price); // 61.62
-            }
-        }
-    }
-}
+//namespace DataCollector.TinkoffAdapterRPC
+//{
+//    internal class Class1
+//    {
+//        void Method()
+//        {
+//            string token = File.ReadAllLines("toksan.dll")[0].Trim();
+//            InvestApiClient client = InvestApiClientFactory.Create($"<{token}>");
+//            GetCandlesRequest getCandlesRequest = new GetCandlesRequest() { Figi = "", From = DateTime.Now.AddDays(-1).ToTimestamp(), Interval = CandleInterval._1Min, To = DateTime.Now.ToTimestamp() };
+//            client.MarketData.GetCandlesAsync(getCandlesRequest);
+//        }
+//    }
+//}
