ce97a92486e2274980339757d9a4ba4f2de031fe
BlankaKorvo <BlankaKorvo@gmail.com>
2023-07-18 13:35:26 +0300

    Update ComparatorTinkoffCandles.cs

--- DataCollector/TinkoffAdapterLegacy/ComparatorTinkoffCandles.cs
+++ DataCollector/TinkoffAdapterLegacy/ComparatorTinkoffCandles.cs
@@ -5,8 +5,29 @@ using Tinkoff.Trading.OpenApi.Models;
 
 namespace DataCollector.TinkoffAdapterLegacy
 {
-    class ComparatorTinkoffCandlesLegasy : IEqualityComparer<CandlePayload>
+    class TinkoffModelComparators : IEqualityComparer<CandlePayload>, IEqualityComparer<HistoricCandle>
     {
+        public bool Equals(HistoricCandle c1, HistoricCandle c2)
+        {
+            Log.Debug("Start Equals method");
+            if (c1.Time == c2.Time)
+            {
+                Log.Debug("{0} {1} candle = {2} {3} candle", c1.Time, c2.Time);
+                return true;
+            }
+            else
+            {
+                Log.Debug("{0} {1} candle != {2} {3} candle", c1.Time, c2.Time);
+                return false;
+            }
+        }
+
+        public int GetHashCode(HistoricCandle c)
+        {
+            string hCode = (c.High * c.Low * c.Open * c.Close).ToString() + c.Volume.ToString() + c.Time.ToString();
+            return hCode.GetHashCode();
+        }
+
         public bool Equals(CandlePayload c1, CandlePayload c2)
         {
             Log.Debug("Start Equals method");
@@ -29,27 +50,27 @@ namespace DataCollector.TinkoffAdapterLegacy
         }
     }
 
-    class ComparatorTinkoffCandles : IEqualityComparer<HistoricCandle>
-    {
-        public bool Equals(HistoricCandle c1, HistoricCandle c2)
-        {
-            Log.Debug("Start Equals method");
-            if (c1.Time == c2.Time)
-            {
-                Log.Debug("{0} {1} candle = {2} {3} candle", c1.Time, c2.Time);
-                return true;
-            }
-            else
-            {
-                Log.Debug("{0} {1} candle != {2} {3} candle", c1.Time, c2.Time);
-                return false;
-            }
-        }
+    //class ComparatorTinkoffCandles : IEqualityComparer<HistoricCandle>
+    //{
+    //    public bool Equals(HistoricCandle c1, HistoricCandle c2)
+    //    {
+    //        Log.Debug("Start Equals method");
+    //        if (c1.Time == c2.Time)
+    //        {
+    //            Log.Debug("{0} {1} candle = {2} {3} candle", c1.Time, c2.Time);
+    //            return true;
+    //        }
+    //        else
+    //        {
+    //            Log.Debug("{0} {1} candle != {2} {3} candle", c1.Time, c2.Time);
+    //            return false;
+    //        }
+    //    }
 
-        public int GetHashCode(HistoricCandle c)
-        {
-            string hCode = (c.High * c.Low * c.Open * c.Close).ToString() + c.Volume.ToString() + c.Time.ToString();
-            return hCode.GetHashCode();
-        }
-    }
+    //    public int GetHashCode(HistoricCandle c)
+    //    {
+    //        string hCode = (c.High * c.Low * c.Open * c.Close).ToString() + c.Volume.ToString() + c.Time.ToString();
+    //        return hCode.GetHashCode();
+    //    }
+    //}
 }
